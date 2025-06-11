af6485438431e0ed42e1e7c3b8940dfc94cd44c9
lephuocthanh2000 <lephuocthanhnbk10h@gmail.com>
2025-06-11 15:11:27 +0700

    update client

--- src/controllers/v1/greetingController.js
+++ src/controllers/v1/greetingController.js
@@ -21,7 +21,7 @@ export const greet = async (req, res, next) => {
     } else if (/^FR/.test(language)) {
       defaultAudience = "Monde";
     } else {
-      defaultAudience = "hello+World";
+      defaultAudience = "helloWorld";
     }
 
     res.status(200).json({
